9246a7e082ddde95a94e09455b3c27afffdea498
Aalena <alina.9808169661@gmail.com>
2017-09-03 14:17:03 +0545

    Changed the HTML for the site

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+#This regular expression will match ^ (a beginning) followed by $ (an end) – so only an empty string will match. That's correct, because in Django URL resolvers, 'http://127.0.0.1:8000/' is not a part of the URL. This pattern will tell Django that views.post_list is the right place to go if someone enters your website at the 'http://127.0.0.1:8000/' address.
+urlpatterns = [
+    url(r'^$',views.post_list,name='post_list'),
+]
